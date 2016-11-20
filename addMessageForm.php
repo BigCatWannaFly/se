05f7a7ac9cb0139a1079ef406669e1561ae01936
AdamCheaw <cheaw0723@gmail.com>
2016-11-20 13:13:43 +0800

    test2

--- addMessageForm.php
+++ addMessageForm.php
@@ -7,6 +7,7 @@ session_start();
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>無標題文件</title>
 </head>
+
 <body>
 <h1>Add New Message</h1>
 <form method="post" action="controller.php">
@@ -17,7 +18,7 @@ session_start();
 
       Author: <input name="myname" type="text" id="myname" /> <br>
 	  
-      <input type="submit" name="Submit" value="送出" />
+      <input type="submit" name="Submit" value="確認" />
 	</form>
   </tr>
 </table>
